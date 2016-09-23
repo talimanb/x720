2728c63e61e93cd1d35a7f68d047313faabf26f6
git-zhanhan.cn.gmail.com <zhanhan.cn@gmail.com>
2016-09-23 15:01:47 +0800

    Disable VL53L0 ldo.

--- drivers/input/misc/vl53L0/stmvl53l0_module-i2c.c
+++ drivers/input/misc/vl53L0/stmvl53l0_module-i2c.c
@@ -167,14 +167,14 @@ static struct i2c_driver stmvl53l0_driver = {
 int stmvl53l0_power_up_i2c(void *i2c_object, unsigned int *preset_flag)
 {
 	int ret = 0;
-#ifndef STM_TEST
+#if 0 //ndef STM_TEST
 	struct i2c_data *data = (struct i2c_data *)i2c_object;
 #endif
 
 	vl53l0_dbgmsg("Enter\n");
 
 	/* actual power on */
-#ifndef STM_TEST
+#if 0 //ndef STM_TEST
 	ret = regulator_set_voltage(data->vana,	VL53L0_VDD_MIN, VL53L0_VDD_MAX);
 	if (ret < 0) {
 		vl53l0_errmsg("set_vol(%p) fail %d\n", data->vana , ret);
@@ -197,12 +197,12 @@ int stmvl53l0_power_up_i2c(void *i2c_object, unsigned int *preset_flag)
 int stmvl53l0_power_down_i2c(void *i2c_object)
 {
 	int ret = 0;
-#ifndef STM_TEST
+#if 0 //ndef STM_TEST
 	struct i2c_data *data = (struct i2c_data *)i2c_object;
 #endif
 
 	vl53l0_dbgmsg("Enter\n");
-#ifndef STM_TEST
+#if 0 //ndef STM_TEST
 	msleep(3);
 	ret = regulator_disable(data->vana);
 	if (ret < 0)
